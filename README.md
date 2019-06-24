a2ef539fba4c2f7ee003cc0bec87c9894b7173b8
markkiths <markkiths@gmail.com>
2019-06-25 02:29:13 +0700

    new vendor

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+git clone https://github.com/markkiths/android_vendor_lava_iris700.git -b cm-14.1 vendor/lava/iris700
+```
+
---
+++ iris700-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lava/iris700/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lava/iris700/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lava/iris700/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lava/iris700/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lava/iris700/proprietary/xbin,system/xbin)
+
